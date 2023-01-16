702a8fea6fbb57b720ad65521caa08da3b9f16d7
AleksaGK <aleksamileticgk@gmail.com>
2023-01-16 21:33:52 +0100

    ostatak casa incompleted, completed i patch

--- controller/task.php
+++ controller/task.php
@@ -73,11 +73,159 @@ if (isset($_GET['taskid'])) {
         $response->addMessage("Task deleted.");
         $response->send();
         exit;
+    } elseif ($_SERVER['REQUEST_METHOD'] === 'PATCH') {
+        //ovde pisemo kod za patch
+        //trebamo da pronadjemo element sa tim id-jem
+        //trebamo da azuriramo samo one podatke koje je korisnik uneo
+        if ($_SERVER['CONTENT_TYPE'] !== 'application/json') {
+            $response = new Response();
+            $response->setHttpStatusCode(400);
+            $response->setSuccess(false);
+            $response->addMessage('Content type header not set to JSON');
+            $response->send();
+            exit;
+        }
+
+        $rawPatchData = file_get_contents('php://input');
+        if (!$jsonData = json_decode($rawPatchData)) {
+            $response = new Response();
+            $response->setHttpStatusCode(400);
+            $response->setSuccess(false);
+            $response->addMessage("Request body is not valid JSON");
+            $response->send();
+            exit;
+        }
+
+        $query = "SELECT * FROM tasks WHERE id=$taskid";
+        $result = $conn->query($query);
+
+        $rowCount = $result->num_rows;
+        if ($rowCount === 0) {
+            $response = new Response();
+            $response->setHttpStatusCode(404);
+            $response->setSuccess(false);
+            $response->addMessage("No task found to update");
+            $response->send();
+            exit;
+        }
+
+        $title_update = false;
+        $description_update = false;
+        $deadline_update = false;
+        $completed_update = false;
+
+        $queryFields = "";
+        if (isset($jsonData->title)) {
+            $title_update = true;
+            $queryFields .= "title='$jsonData->title',";
+        }
+        if (isset($jsonData->description)) {
+            $description_update = true;
+            $queryFields .= "description='$jsonData->description',";
+        }
+        if (isset($jsonData->deadline)) {
+            $deadline_update = true;
+            $queryFields .= "deadline='$jsonData->deadline',";
+        }
+        if (isset($jsonData->completed)) {
+            $completed_update = true;
+            $queryFields .= "completed='$jsonData->completed',";
+        }
+
+        if ($title_update === false && $description_update === false && $deadline_update === false && $completed_update === false) {
+            $response = new Response();
+            $response->setHttpStatusCode(400);
+            $response->setSuccess(false);
+            $response->addMessage("No task fields provided");
+            $response->send();
+            exit;
+        }
+        $queryFields = rtrim($queryFields, ",");
+        var_dump($queryFields);
+        $queryString = "UPDATE tasks SET $queryFields WHERE id=$taskid";
+        $result2 = $conn->query($queryString);
+
+        //sta ako nije lepo azuriran red u tabeli
+
+        //potrebno je napisati upit koji proverava da li postoji element sa: id, deadline, completed, title, description
+
+        $result4 = $conn->query("SELECT * FROM tasks WHERE id=$taskid");
+        $rowCount = $result4->num_rows;
+        if ($rowCount === 0) {
+            $response = new Response();
+            $response->setHttpStatusCode(404);
+            $response->setSuccess(false);
+            $response->addMessage("No task found");
+            $response->send();
+            exit;
+        }
+
+        $row = $result4->fetch_assoc();
+        $task = new Task($row['id'], $row['title'], $row['description'], $row['deadline'], $row['completed']);
+
+        $taskArray[] = $task->returnTaskArray();
+        $returnData = array();
+        $returnData['row_returned'] = $rowCount;
+        $returnData['task'] = $taskArray;
+
+        $response = new Response();
+        $response->setHttpStatusCode(200);
+        $response->setSuccess(true);
+        $response->addMessage('Task updated');
+        $response->setData($returnData);
+        $response->send();
+        exit;
     } else {
         $response = new Response();
         $response->setHttpStatusCode(405);
         $response->setSuccess(false);
         $response->addMessage('Request method not allowed');
+        $response->send();
+        exit;
+    }
+} elseif (isset($_GET['completed'])) {
+    $completed = $_GET['completed'];
+
+    if ($completed !== 'Y' && $completed !== 'N') {
+        $response = new Response();
+        $response->setHttpStatusCode(400);
+        $response->setSuccess(false);
+        $response->addMessage("Completed filter must be Y or N");
+        $response->send();
+        exit;
+    }
+    if ($_SERVER['REQUEST_METHOD'] === "GET") {
+        $query = "SELECT * FROM tasks WHERE completed='$completed'";
+        $result = $conn->query($query);
+        $rowCount = $result->num_rows;
+        if ($rowCount === 0) {
+            $response = new Response();
+            $response->setHttpStatusCode(404);
+            $response->setSuccess(false);
+            $response->addMessage("No task found");
+            $response->send();
+            exit;
+        }
+
+        while ($row = $result->fetch_assoc()) {
+            $task = new Task($row['id'], $row['title'], $row['description'], $row['deadline'], $row['completed']);
+            $taskArray[] = $task->returnTaskArray();
+        }
+
+        $returnData = array();
+        $returnData['row_returned'] = $rowCount;
+        $returnData['tasks'] = $taskArray;
+        $response = new Response();
+        $response->setHttpStatusCode(200);
+        $response->setSuccess(true);
+        $response->setData($returnData);
+        $response->send();
+        exit;
+    } else {
+        $response = new Response();
+        $response->setHttpStatusCode(405);
+        $response->setSuccess(false);
+        $response->addMessage("Request method not allowed");
         $response->send();
         exit;
     }
